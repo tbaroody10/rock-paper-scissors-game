710f0f6e7770e309fa4a09d54dcf4df53412e9ee
tbaroody10 <tcb81@georgetown.edu>
2021-09-21 11:43:30 -0400

    Update game.py

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ print("Rock, Paper, Scissors, Shoot!")
 
 #x = input("Choose 'rock' or 'paper' or 'scissors'")
 user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+print("User chose:")
 print(user_choice)
 
 # computer choice (at random)
@@ -15,4 +16,11 @@ print(user_choice)
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
+print("Computer chose:")
 print(computer_choice)
+
+
+breakpoint()
+
+
+print("THANKS PLEASE PLAY AGAIN")
